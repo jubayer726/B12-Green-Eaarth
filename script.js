9e663e2072ee631b5b608b18e7d3c76ee231aef2
JUBAYER <juelali482@gmail.com>
2025-09-08 00:42:43 +0600

    added functin of Categories

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("JavaScript Connected");
+
+const categoryContainer = document.getElementById("category-container")
+
+const loadCategory = () => {
+    fetch("https://openapi.programming-hero.com/api/categories")
+    .then((res) => res.json())
+    .then(data => {
+        const categories = data.categories
+        showCategory(categories)
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
+
+const showCategory = (categories) =>{
+    categories.forEach(cat => {
+        categoryContainer.innerHTML += `
+            <div class="text-gray-500 hover:bg-green-600 rounded-lg p-1 hover:text-white">${cat.category_name}</div>
+        `;
+    });
+
+    categoryContainer.addEventListener("click", (e) => {
+        showCardByCategoriy(e.target.id)
+        console.log(e.target.id);
+    })
+}
+
+const showCardByCategoriy = (categoryID) => {
+    fetch(`https://openapi.programming-hero.com/api/category/${categoryID}`)
+    .then((res) => res.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
+
+
+
+loadCategory();
