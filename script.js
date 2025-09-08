c7288f98916fe1ffa7b34a31e5e95323ee5d9955
JUBAYER <juelali482@gmail.com>
2025-09-08 18:17:58 +0600

    added more fuction

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 const categoryContainer = document.getElementById("category-container");
 const cardContainer = document.getElementById("card-container");
+const cartContainer = document.getElementById("cart-container")
+let carts = [];
 
 const loadCategory = () => {
     fetch("https://openapi.programming-hero.com/api/categories")
@@ -14,6 +16,7 @@ const loadCategory = () => {
     });
 }
 
+        //Category Section ///
 const showCategory = (categories) =>{
     categories.forEach(cat => {
         categoryContainer.innerHTML += `
@@ -37,11 +40,10 @@ const showCategory = (categories) =>{
 }
 
 const loadCardByCategoriy = (categoryID) => {
-    console.log(categoryID);
     fetch(`https://openapi.programming-hero.com/api/category/${categoryID}`)
     .then((res) => res.json())
     .then(data => {
-        // console.log(data.plants);
+        
         showCardByCategory(data.plants)
     })
     .catch(err => {
@@ -49,14 +51,16 @@ const loadCardByCategoriy = (categoryID) => {
     });
 }
 
+        //Card Section  ////
 const showCardByCategory = (card) => {
-    // console.log(card);
     cardContainer.innerHTML = "";
     card.forEach(cards => {
         cardContainer.innerHTML += `
-                <div class="p-3 rounded-2xl bg-white">
+                <div class="p-3 rounded-2xl bg-white shadow-sm">
                     <img class="rounded-xl" src="${cards.image}" alt="photo">
+                        <div id ="${cards.id}">
                         <h2 class="text-2xl font-semibold">${cards.name}</h2>
+                        </div>
                         <p class="text-gray-500 py-2">${cards.description}</p>
                         <div class="flex justify-between py-4">
                             <h3 class="bg-green-200 text-green-700 px-3 rounded-full">${cards.category}</h3>
@@ -70,4 +74,47 @@ const showCardByCategory = (card) => {
 }
 
 
-loadCategory();
+///  Cart Load///
+cardContainer.addEventListener("click", (e) => {
+    if(e.target.innerText === "Add to Cart"){
+        handleCarts(e)
+    }
+})
+const handleCarts = (e) => {
+    const title = e.target.parentNode.children[1].innerText
+       const id = e.target.parentNode.children[1].id
+       const price = e.target.parentNode.children[3].children[1].children[0].innerText;
+       carts.push({
+        id: id,
+        title:title,
+        price:price
+       });
+       showCarts(carts)
+}
+const showCarts = (carts) => {
+    console.log(carts);
+    cartContainer.innerHTML = "";
+    carts.forEach(cart => {
+        cartContainer.innerHTML += `
+            <div class="bg-green-50 rounded-lg m-3 p-2 space-y-2">
+                    <h3 class="">${cart.title}</h3>
+                    <div class="flex justify-between items-center">
+                        <p class="text-gray-500 text-sm">৳<span>${cart.price}</span> x <span>1</span></p>
+                        <button onclick="cartDeleteBtn('${cart.id}')">x</button>
+                    </div>
+            </div>
+        `;
+    }) 
+}
+
+/// Cart Delete Button ////
+
+const cartDeleteBtn = (cartID) => {
+   console.log(cartID);
+   const filterCarts = carts.filter(cart => cart.id !== cartID)
+   carts = filterCarts
+   showCarts(carts)
+}
+
+loadCategory()
+loadCardByCategoriy('1')
